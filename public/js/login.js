edf1c66f3ff5927d1315220513e301a00b40ffc0
Artur Sopov <tullers@mail.ru>
2022-05-05 15:34:23 +0300

    upd

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,8 @@ const login = async (email, password) => {
   console.log(email, password);
 
   try {
-    const URL = `http://natours:3000/api/v1/users/login`;
+    // const URL = `http://127.0.0.1:3000/api/v1/users/login`;
+    const URL = `http://172.18.09:3000/api/v1/users/login`;
     const res = await fetch(URL, {
       method: 'POST',
       headers: {
